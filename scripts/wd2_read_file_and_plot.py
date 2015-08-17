c8c88266138ec254f2b32154db17ce13dca50aa2
Elmar Schmid <elmar.schmid@psi.ch>
2015-08-17 14:35:19 +0200

    Removed duplicated lines in script output for status registers in .h file.

--- scripts/wd2_read_file_and_plot.py
+++ scripts/wd2_read_file_and_plot.py
@@ -42,18 +42,20 @@ ch_data_mV    = np.zeros((adcs, channels,frame_size), dtype=np.int32)
 for d in data:
   if d[i_data_seq_nr] != current_frame:
     print "Frame Error (" + str(current_frame) + ", " + str(d[i_data_seq_nr]) + ")"
-    quit()
+    #quit()
+    break;
   adc     = d[i_header_adc]
   ch      = d[i_header_ch]
   segment = d[i_ch_segment_nr]
-  ch_data_value[adc, ch, segment*frame_size/2:(segment+1)*frame_size/2] = d[data_offset:]                            # ADC Value
-  ch_data_mV[adc, ch, segment*frame_size/2:(segment+1)*frame_size/2]    = d[data_offset:]*1000*v_range/(2**res_bits) # mV
+  ch_data_value[adc, ch, segment*frame_size/2:(segment+1)*frame_size/2] = d[data_offset:(data_offset+frame_size/2)]                            # ADC Value
+  ch_data_mV[adc, ch, segment*frame_size/2:(segment+1)*frame_size/2]    = d[data_offset:(data_offset+frame_size/2)]*1000*v_range/(2**res_bits) # mV
 
 #print "Data read:", data
 #print "Data size:", np.size(data)
 
 time_step_us = 1.0/sampling_freq
-time = np.arange(1024)*time_step_us
+#time = np.arange(1024)*time_step_us
+time = np.arange(1024)
 
 print "Plotting Frame " + str(current_frame) + "...\n"
 for adc in np.arange(adcs):
@@ -65,13 +67,17 @@ for adc in np.arange(adcs):
     rms_noise_bits = np.std(ch_data_value[adc, ch])
     pl.title("ADC/Channel: %d/%d   Frame: %d   Noise = %.3f LSBs = %.3f mV" % (adc, ch, current_frame, rms_noise_bits, rms_noise_mV))
     pl.plot(time, ch_data_mV[adc, ch])
+    #pl.plot(time, ch_data_value[adc, ch])
     pl.grid()
     if ch > 5:
-      pl.xlabel("time [us]")
-    #pl.ylabel("ADC value")
+#      pl.xlabel("time [us]")
+      pl.xlabel("sample")
     pl.ylabel("Amplitude [mV]")
-    pl.xlim(0 , 12.5)
+    #pl.ylabel("ADC value")
+#    pl.xlim(0 , 12.5)
+    pl.xlim(0 , 1023)
     pl.ylim(np.amin(ch_data_mV[adc, ch])-10,np.amax(ch_data_mV[adc, ch])+10)
+    #pl.ylim(np.amin(ch_data_value[adc, ch])-10,np.amax(ch_data_value[adc, ch])+10)
 
     print "RMS Noise ADC/Channel %d/%d: %.3f LSBs = %.3f mV" % (adc, ch, rms_noise_bits, rms_noise_mV)
     #print "RMS Noise ADC " + str(adc) + " Channel " + str(ch) + ": " + str(rms_noise_mV) + "mV"
