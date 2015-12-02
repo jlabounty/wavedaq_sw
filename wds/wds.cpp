13a393f1f23504440cfd9845325f54995f19e827
Stefan Ritt <stefan.ritt@psi.ch>
2015-12-02 14:14:29 +0100

    Use "adcgeteth 1" to obtain ADC data, changed "drsstart/ststrig" to "drsget"

--- wds/wds.cpp
+++ wds/wds.cpp
@@ -66,11 +66,10 @@ static int wds_handler(struct mg_connection *conn, enum mg_event event)
 
          if (gl->adc_flag) {
             // issue single ADC software trigger
-            interface_send(gl, board, 100, "adcget 1\n", NULL, NULL);
+            interface_send(gl, board, 100, "adcgeteth 1\n", NULL, NULL);
          } else {
             // issue single DRS software trigger
-            interface_send(gl, board, 100, "drsstart\n", NULL, NULL);
-            interface_send(gl, board, 100, "drstrig\n", NULL, NULL);
+            interface_send(gl, board, 100, "drsget\n", NULL, NULL);
          }
          // read waveforms
          status = interface_read_waveform(gl, board, 1000, wfU);
