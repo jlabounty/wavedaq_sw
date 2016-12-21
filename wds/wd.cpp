a7ddbff773712d1422e382299df983a0f9aa8c9e
Stefan Ritt <stefan.ritt@psi.ch>
2016-12-21 09:46:18 +0100

    Removed strlcpy (now in separate file)

--- wds/wd.cpp
+++ wds/wd.cpp
@@ -94,59 +94,6 @@ void remove_spikes(GLOBALS *gl, short trigger_cell, float wf[][1024]);
 
 /*-----------------------------------------------------------------------------------------*/
 
-size_t strlcpy(char *dst, const char *src, size_t size)
-{
-   char *d = dst;
-   const char *s = src;
-   size_t n = size;
-   
-   /* Copy as many bytes as will fit */
-   if (n != 0 && --n != 0) {
-      do {
-         if ((*d++ = *s++) == 0)
-            break;
-      } while (--n != 0);
-   }
-   
-   /* Not enough room in dst, add NUL and traverse rest of src */
-   if (n == 0) {
-      if (size != 0)
-         *d = '\0';             /* NUL-terminate dst */
-      while (*s++);
-   }
-   
-   return (s - src - 1);        /* count does not include NUL */
-}
-
-size_t strlcat(char *dst, const char *src, size_t size)
-{
-   char *d = dst;
-   const char *s = src;
-   size_t n = size;
-   size_t dlen;
-   
-   /* Find the end of dst and adjust bytes left but don't go past end */
-   while (n-- != 0 && *d != '\0')
-      d++;
-   dlen = d - dst;
-   n = size - dlen;
-   
-   if (n == 0)
-      return (dlen + strlen(s));
-   while (*s != '\0') {
-      if (n != 1) {
-         *d++ = *s;
-         n--;
-      }
-      s++;
-   }
-   *d = '\0';
-   
-   return (dlen + (s - src));   /* count does not include NUL */
-}
-
-/*-----------------------------------------------------------------------------------------*/
-
 void GetTimeStamp(TIMESTAMP &ts)
 {
 #ifdef _MSC_VER
