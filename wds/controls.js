36458e8ed8126a7c9450c9a213ed24bcf31f6165
Stefan Ritt <stefan.ritt@psi.ch>
2019-02-05 13:51:30 +0100

    Position dialog boxed in middle of screen, independent of scrolling of underlying panel

--- wds/controls.js
+++ wds/controls.js
@@ -448,9 +448,10 @@ function dlgShow(dlg, modal) {
    d.dlgAy = 0;
    d.dlgDx = 0;
    d.dlgDy = 0;
-   d.modal = (modal !== undefined);
+   d.modal = (modal === true);
 
    d.style.display = "block";
+   d.style.position = "fixed";
    d.style.left = Math.round(document.documentElement.clientWidth / 2 - d.offsetWidth / 2) + "px";
    if (document.documentElement.clientHeight / 2 - d.offsetHeight / 2 < 0)
       d.style.top = "0px";
