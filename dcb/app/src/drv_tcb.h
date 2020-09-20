95290dae106e49839c7d30afead63a9b4b8d6ff3
Marco Francesconi <marco.francesconi@pi.infn.it>
2020-09-20 18:41:50 +0200

    dcbs now breaks TCB banks into smaller packets

--- dcb/app/src/drv_tcb.h
+++ dcb/app/src/drv_tcb.h
@@ -28,6 +28,8 @@
 #define DATA_TYPE_DUMMY 5
 #define DATA_TYPE_TCB 8
 
+#define MAXWORDSINPACKET 200
+
 //typedefs
 #pragma pack(push, 1) // byte-level alignment
 
@@ -75,7 +77,7 @@ void readBlock(int slot, WDAQ_BRD* board, unsigned int addr, unsigned short size
 void writeReg(int slot, WDAQ_BRD *board, unsigned int addr, unsigned int val);
 
 //packet sender
-void sendPacket(unsigned int slot, unsigned int pkgnum, unsigned int npkg, TcbSpiBufferHeader* bufferhead, TcbSpiBankHeader* bankhead, unsigned int *data);
+void sendPacket(unsigned int slot, unsigned int* pkgnum, unsigned int ibank, unsigned int nbank, TcbSpiBufferHeader* bufferhead, TcbSpiBankHeader* bankhead, unsigned int *data);
 
 //endianess correction before sending
 void correctEndianness(WdaqUdpPacketHeader* wdaqheader, TcbUdpPacketHeader* tcbheader);
