e6f1fa8dfd996aa6a2c9aa148c64dab33673cfd1
Elmar Schmid <elmar.schmid@psi.ch>
2020-11-12 16:32:18 +0100

    Change from single shot to test loop

--- dcb/drivers/dma_packet_scheduler_v1_0/src/dps_test.c
+++ dcb/drivers/dma_packet_scheduler_v1_0/src/dps_test.c
@@ -1,8 +1,10 @@
 #include "dma_packet_scheduler_v1_0.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <time.h>
 #include <sys/ioctl.h>
 
 #include <string.h>
@@ -20,6 +22,22 @@
 #define EMS_RH7_LC_IP   (inet_addr("129.129.188.245"))
 #define MAXLINE         1024
 #define BUF_SIZE        4096
+
+struct wdaq_header
+{
+  uint8_t  protocol_version;
+  uint8_t  board_type_revision;
+  uint16_t serial_number;
+  uint8_t  crate_id;
+  uint8_t  slot_id;
+  uint16_t packet_number;
+  uint8_t  data_type;
+  uint8_t  wdaq_flags;
+  uint16_t trigger_information[3];
+  uint32_t event_number;
+  uint16_t payload_length;
+  uint16_t data_chunk_offset;
+};
 
 void usage(char *name)
 {
@@ -72,11 +90,30 @@ int release_buffer_sysfs (int n_buffers)
   return 0;
 }
 
+void print_header(struct wdaq_header * wdaq_header)
+{
+  printf("\nPacket header:\n");
+  printf("protocol version:       0x%02X (%d)\n", wdaq_header->protocol_version, wdaq_header->protocol_version);
+  printf("board type revision:    0x%02X (%d)\n", wdaq_header->board_type_revision, wdaq_header->board_type_revision);
+  printf("serial number:          0x%04X (%d)\n", ntohs(wdaq_header->serial_number), ntohs(wdaq_header->serial_number));
+  printf("crate id:               0x%02X (%d)\n", wdaq_header->crate_id, wdaq_header->crate_id);
+  printf("slot id:                0x%02X (%d)\n", wdaq_header->slot_id, wdaq_header->slot_id);
+  printf("packet number:          0x%04X (%d)\n", ntohs(wdaq_header->packet_number), ntohs(wdaq_header->packet_number));
+  printf("data type:              0x%02X (%d)\n", wdaq_header->data_type, wdaq_header->data_type);
+  printf("wdaq flags:             0x%02X (%d)\n", wdaq_header->wdaq_flags, wdaq_header->wdaq_flags);
+  printf("trigger information[0]: 0x%04X (%d)\n", ntohs(wdaq_header->trigger_information[0]), ntohs(wdaq_header->trigger_information[0]));
+  printf("trigger information[1]: 0x%04X (%d)\n", ntohs(wdaq_header->trigger_information[1]), ntohs(wdaq_header->trigger_information[1]));
+  printf("trigger information[2]: 0x%04X (%d)\n", ntohs(wdaq_header->trigger_information[2]), ntohs(wdaq_header->trigger_information[2]));
+  printf("event number:           0x%08X (%d)\n", ntohl(wdaq_header->event_number), ntohl(wdaq_header->event_number));
+  printf("payload length:         0x%04X (%d)\n", ntohs(wdaq_header->payload_length), ntohs(wdaq_header->payload_length));
+  printf("data chunk offset:      0x%04X (%d)\n", ntohs(wdaq_header->data_chunk_offset), ntohs(wdaq_header->data_chunk_offset));
+}
+
 int read_data (void)
 {
   int ret;
   unsigned int len;
-  FILE *f;
+//  FILE * f;
   int fd;
   char buffer[BUF_SIZE];
   int sockfd;
@@ -88,12 +125,16 @@ int read_data (void)
   int ready;
   int read_len;
   int fpos;
+  time_t sys_time;
+  struct tm loc_time;
+  struct wdaq_header * wdaq_header;
 
   len = 0;
 
 #ifdef DO_UDP_SEND
   // Creating socket file descriptor
-  if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+  if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 )
+  {
       perror("socket creation failed");
       exit(EXIT_FAILURE);
   }
@@ -116,12 +157,15 @@ int read_data (void)
 
   ready = select(nfds, &readfds, NULL, NULL, NULL);
 
-  // TODO: Report system time !!!
+  sys_time = time(NULL);
+  loc_time = *localtime(&sys_time);
 
   //len = get_bufsize_ioctl(fd);
   len = get_bufsize_sysfs();
-  printf("\nFile size %d bytes\n", len);
+  printf("%d.%02d.%02d %02d:%02d:%02d - ", loc_time.tm_mday, loc_time.tm_mon + 1, loc_time.tm_year + 1900, loc_time.tm_hour, loc_time.tm_min, loc_time.tm_sec);
+  printf("packet size %d bytes, ", len);
 
+#if 0
   /* Test read and lseek */
   printf("\nUser Reading File (%d bytes):\n", len);
   read_len = len/4;
@@ -141,13 +185,26 @@ int read_data (void)
     for(i=0;i<ret;i++) printf(" %02X", buffer[i]);
   }
   printf("\n");
+#endif
 
-#if 0
   /* Test mmap */
-  printf("\nUser Reading Memory Mapped Data:");
   data = mmap(NULL, BUF_SIZE, PROT_READ, MAP_SHARED, fd, 0);
   if(data != MAP_FAILED)
   {
+    wdaq_header = (struct wdaq_header *)data;
+
+    printf("slot 0x%02X, ",   wdaq_header->slot_id);
+    printf("packet nr %d, ",  ntohs(wdaq_header->packet_number));
+    printf("event nr %d, ",   ntohl(wdaq_header->event_number));
+    printf("trigger nr %d, ", ntohs(wdaq_header->trigger_information[2]));
+    printf("trigger type %d", ntohs(wdaq_header->trigger_information[0]));
+    printf("\n");
+
+#if 0
+    print_header(wdaq_header);
+#endif
+
+#if 0
     for(i = 0; i<len; i++)
     {
       if( (i%16)==0 ) printf("\n");
@@ -168,17 +225,23 @@ int read_data (void)
     {
       printf("Unmap failed");
     }
-
-//    if (release_buffer_ioctl(fd) == 0)
-    if (release_buffer_sysfs(1) == 0)
-    {
-      printf("\nCurrent window buffer released\n");
-    }
   }
   else
   {
     printf("No data available\n");
   }
+
+  if (0 != release_buffer_sysfs(1))
+  {
+    printf("\n Failed to release current window buffer\n");
+  }
+#if 0
+//  if (release_buffer_ioctl(fd) == 0)
+  if (release_buffer_sysfs(1) == 0)
+  {
+    printf("\nCurrent window buffer released\n");
+  }
+#endif
 
   return close(fd);
 }
