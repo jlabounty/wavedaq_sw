8a7190dc3668197706e38f00c2425f432ecedd22
Marco Francesconi <marco.francesconi@pi.infn.it>
2021-07-20 12:10:51 +0200

    pedestal configuration for 2021

--- src/WDLib.cpp
+++ src/WDLib.cpp
@@ -981,11 +981,7 @@ void WDWDB::ConfigurationStarted(){
 
    SetFeMux(-1, WDB::cFeMuxInput);
    SetTriggerOutPulseLength(4); // 4 clock shaping
-   //SetAdvTrgPedCfg(0x0124000A); // default pedestal subtraction config
-   //SetAdvTrgPedCfg(0x0120000A); // default pedestal subtraction config
-   //SetAdvTrgPedCfg(0x010A000A); // default pedestal subtraction config
-   //SetAdvTrgPedCfg(0x010A0018); // default pedestal subtraction config
-   SetAdvTrgPedCfg(0x0210000A); // default pedestal subtraction config
+   SetAdvTrgPedCfg(0x042B0004); // default pedestal subtraction config
 }
 
 void WDWDB::ConfigurationEnded(){
